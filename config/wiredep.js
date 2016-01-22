a8b428f5f8af5ffe9665abf284cb3dec4e740bea
David Kebler <d@kebler.net>
2016-01-22 15:15:25 -0800

    add mdl for testing

--- config/wiredep.js
+++ config/wiredep.js
@@ -1,9 +1,9 @@
 var config = require('./') // config/index.js
-var bowerjson = require(config.repoRoot + "/bower.json"); 
+var bowerjson = require(config.repoRoot + "/bower.json");
 var bowerrc = require('jsonfile').readFileSync(config.repoRoot + "/.bowerrc");
 
 module.exports = {
 directory: bowerrc.directory,
 bowerJson: bowerjson,
-  }
 
+  }
