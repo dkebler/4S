b6a44b47291d96637f01a8e614bb5603eb131106
David Kebler <d@kebler.net>
2016-02-23 10:09:25 -0800

    switch to njk add html beautify

--- gulpfile.js
+++ gulpfile.js
@@ -153,16 +153,41 @@ gulp.task('test', function() {
 
 var merge = require('gulp-file-include')
 var rename = require("gulp-rename");
+var htmlbeautify = require('gulp-html-beautify');
+
+
+var hbOptions = {
+    "indent_size": 4,
+    "indent_char": " ",
+    "eol": "\n",
+    "indent_level": 0,
+    "indent_with_tabs": false,
+    "preserve_newlines": false,  // changed
+    "max_preserve_newlines": 10,
+    "jslint_happy": false,
+    "space_after_anon_function": false,
+    "brace_style": "collapse",
+    "keep_array_indentation": false,
+    "keep_function_indentation": false,
+    "space_before_conditional": true,
+    "break_chained_methods": false,
+    "eval_code": false,
+    "unescape_strings": false,
+    "wrap_line_length": 0,
+    "wrap_attributes": "auto",
+    "wrap_attributes_indent_size": 4,
+    "end_with_newline": false
+}
+
+// Info(hbOptions)
 
 gulp.task('html:merge', function() {
 
-  gulp.src(['assets/html/base/layouts/*.pnjs'])
-    .pipe(merge({
-      prefix: '@@',
-      basepath: 'assets/html/base/'
-    }))
-    .pipe(rename({extname: ".njs"}))
-    .pipe(gulp.dest('assets/html/base/layouts/'));
+  gulp.src(['assets/html/base/layouts/*.pnjk'])
+    .pipe(merge({prefix: '@@',basepath: 'assets/html/base/'}))
+    .pipe(rename({extname: ".njk"}))
+   .pipe(htmlbeautify(hbOptions))
+   .pipe(gulp.dest('assets/html/base/layouts/'));
 });
 
 
@@ -172,7 +197,7 @@ var nunjucks = require('gulp-nunjucks-render');
 
 gulp.task('html:extend',['html:merge'], function() {
 
-	gulp.src('assets/html/base/*.njs')
+	gulp.src('assets/html/base/*.njk')
 		.pipe(nunjucks({ path: ['assets/html/base'] // String or Array
     }))
 			.on('error', console.log)
