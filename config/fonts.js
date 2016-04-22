965d36636e90b5883716412ee27464ba9c9a430f
David Kebler <d@kebler.net>
2016-04-22 10:33:26 -0700

    move html processing to lib directory, begin rework of lib files to remove gulp task

--- config/fonts.js
+++ config/fonts.js
@@ -1,6 +1,2 @@
-var config = require('./')
-
 module.exports = {
-  src: Config.sourceAssets + '/fonts/**/*',
-  dest: Config.publicAssets + '/fonts'
 }
--- config/index.js
+++ config/index.js
@@ -1,22 +1,28 @@
-var config = {}
-
+module.exports = {
 // If you want to move this file or rename it you can do that with a 'configfilepath' entry in package.json
 // It must be the "path/to/filename".  If absent it will default to config/index.js
 // TODO Deal with ./ , following / and relative paths in a more flexible way using npm relative module.
 // All directory paths in this form  "./path/to/place/"
-config.repoRoot = require('app-root-path').path; // root of repo/project in the file system, used for absolute references
-config.configDirectory = './config/';  //  directory where collection of config files resides.
-config.buildDirectory = './builds/';  // where the various builds are put
-config.buildSubdirectory  = {dev:'dev/', dist:'dist/'};  // different builds within .builds
-config.libDirectory = './lib/';  // workflow library directory
-config.assetsDirectory = "./assets/";  // location of font, css, site images, html templates
-config.contentDirectory = "./content/"
-// content files are usually generator specific so put in subdirectory so it's clear (e.g. content/hugo)
-config.htmlDirectory ="./assets/html/"; // html templates and other layout and compnents
-// put generator templates/files in subdirectory of config.htmlDirectory of the htmlGenerator (e.g. html/hugo)
-config.htmlGenerator = "hugo"; // specify which static site generator is being used.
-config.buildType  = 'dev';  // set this to either 'dev' (default) or 'dist'
-config.localport = '8090'; // default port number for localhost serving (dev)
-config.url = 'localhost:' + config.localport; // default url for for development
 
-module.exports = config;
+  repoRoot : require('app-root-path').path, // root of repo/project in the file system, used for absolute references
+  configDirectory: 'config',  //  directory where collection of config files resides.
+  buildDirectory: 'builds',  // where the various builds are put
+  buildSubdirectory : {dev:'dev/', dist:'dist/'},  // different builds within .builds
+  libDirectory: 'lib',  // workflow library directory
+  assetsDirectory: 'assets',  // location of font, css, site images, html templates
+  contentDirectory: 'content',  // content files are usually generator specific so put in subdirectory so it's clear (e.g. content/hugo)
+  htmlGenerator: 'hugo', // specify which static site generator is being used and directory of its templates and extension file
+  buildType : 'dev',  // set this to either 'dev' (default) or 'dist'
+  localport: '8090' // port number for localhost serving (dev)
+}
+
+//   repoRoot = require('app-root-path').path; // root of repo/project in the file system, used for absolute references
+//   configDirectory = 'config';  //  directory where collection of config files resides.
+//   buildDirectory = 'builds';  // where the various builds are put
+//   buildSubdirectory  = {dev:'dev/', dist:'dist/'};  // different builds within .builds
+//   libDirectory = 'lib';  // workflow library directory
+//   assetsDirectory = 'assets';  // location of font, css, site images, html templates
+//   contentDirectory = 'content'  // content files are usually generator specific so put in subdirectory so it's clear (e.g. content/hugo)
+//   htmlGenerator = 'hugo'; // specify which static site generator is being used and directory of its templates and extension file
+//   buildType  = 'dev';  // set this to either 'dev' (default) or 'dist'
+//   localport = '8090'; // port number for localhost serving (dev)
