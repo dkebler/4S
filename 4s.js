4bd2678fd2cfdbaef4290cccc030a940b599962f
David Kebler <d@kebler.net>
2016-05-05 13:03:17 -0700

    promise based config loading via init function

--- 4s.js
+++ 4s.js
@@ -1,11 +1,35 @@
-// Super Static Starter Entry Point
+// 4S - Super Static Site Starter Entry Point
+'use strict';
+
+let Promise = require('promise');
 
 const repoPath = __dirname + '/';
 const configJSPath = require(repoPath + 'package').configjspath || repoPath + 'config/config.js'; // 'configjspath' is key in package.json.  If not set default is used
 const configPath = require(repoPath + 'package').configpath || repoPath + 'config/default.cson'; // 'configdir' is key in package.json.  If not set /config is the default
 
-let loadconfig = require(configJSPath).load; // alternatively try this https://github.com/snowyu/load-config-file.js
+let load = require(configJSPath).load;
 
-loadconfig(configPath)
-.then(config => console.log('config', config))
-.catch(e => console.log('error', e));
+init().then( config => {
+  // configs loaded ok load the debugger here
+ console.log('the configs from init', config)})
+ .catch(e => console.log('error', e));
+
+//*************************************
+// Intialize 4S Environment
+// loads in main and cli configs and builds cli
+//*******************************************
+
+function init() {
+
+  let loadConfig = load(configPath);
+  let loadCliConfig = loadConfig.then(config => {
+    load(repoPath + config.cliConfigPath);
+    return load(config.cliConfigPath);
+  });
+
+  return Promise.all([loadConfig, loadCliConfig])
+    .then(configs => {
+      configs[0].cliData = configs[1];  // Merge cli configuration data as a key in main config object
+      return configs[0]; // now return merged config file
+    });
+}
--- config/config.js
+++ config/config.js
@@ -1,21 +1,23 @@
 // CONFIG module
-
-let read = require('config').util.parseFile; // alternatively try this https://github.com/snowyu/load-config-file.js
 let Promise = require('promise');
 
+// allows single place to change module used for reading multiple config file types
+let read = require('config').util.parseFile; // TODO switch to  https://github.com/snowyu/load-config-file.js, as it supports promises
+
 module.exports = {
-
-    load: function(configPath, callback) {
-        let done = new Promise(function(resolve, reject) {
-            let config = read(configPath); // master object to hold all site configuration data
-            if (config instanceof Error) {
-              reject(config.stack);
-            } else {
-              resolve(config);
-            }
-          });
-            done.nodeify(callback);
-            return done;
-          }
-
-        }
+  // loads any config type to object hash
+  load: function(filePath, callback) {
+    return new Promise(function(resolve, reject) {
+      let object = read(filePath);
+      if (object instanceof Error) {
+        reject(object.stack);
+      } else {
+        resolve(object);
+      }
+    });
+    done.nodeify(callback);
+  },
+  edit: {},
+  write:{},
+  view:{}
+}
