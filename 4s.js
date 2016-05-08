43d78bfca75fc0ba7ccdd481f37f8a405e697641
David Kebler <d@kebler.net>
2016-05-08 11:07:00 -0700

    loads libraries into master object

--- 4s.js
+++ 4s.js
@@ -2,18 +2,19 @@
 'use strict';
 
 let Promise = require('promise');
-let getlibs = require('require-all') ;
+let getlibs = require('require-all');
 
 const repoPath = __dirname + '/';
 const configJSPath = require(repoPath + 'package').configjspath || repoPath + 'config/config.js'; // 'configjspath' is key in package.json.  If not set default is used
 const configPath = require(repoPath + 'package').configpath || repoPath + 'config/default.cson'; // 'configdir' is key in package.json.  If not set /config is the default
 
 let config = require(configJSPath);
-console.log(config.load);
 
 init().then(config => {
     // configs loaded ok load the debugger here
-    console.log('the configs from init', config)
+    console.log('the configs from init\n', config)
+      // Build the cli
+      // Launch Cli
   })
   .catch(e => console.log('error', e));
 
@@ -26,20 +27,16 @@ function init() {
 
   let getMainConfig = config.load(configPath);
   let getCliConfig = getMainConfig.then(mainConfig => {
-    config.load(repoPath + mainConfig.cliConfigPath);
-    return config.load(mainConfig.cliConfigPath);
+    return config.load(repoPath + mainConfig.cliConfigPath);
+  });
+  let getLibs = getMainConfig.then(mainConfig => {
+    return config.loadLibs(repoPath + mainConfig.dir.lib);
   });
 
-  return Promise.all([getMainConfig, getCliConfig])
+  return Promise.all([getMainConfig, getCliConfig, getLibs])
     .then(configs => {
       configs[0].cliData = configs[1]; // Add cli configuration data as a key in main config object
-        let libs = getlibs({  // load all the repo js modules at once
-        dirname: repoPath + configs[0].dir.lib,
-        filter :/(.*)\.js$/,
-        recursive: false
-      });
-      console.log('libs', libs);
-      configs[0].lib = libs; //Add js libraries to config for easy access
-      return configs[0]; // now return merged config file
+      configs[0].lib = configs[2]; //Add js libraries as a key to main config for easier access
+      return configs[0]; // now return merged main config file
     });
 }
--- config/config.js
+++ config/config.js
@@ -2,13 +2,31 @@
 let Promise = require('promise');
 
 // allows single place to change module used for reading multiple config file types
-let read = require('config').util.parseFile; // TODO switch to  https://github.com/snowyu/load-config-file.js, as it supports promises
+let readConfigFile = require('config').util.parseFile; // TODO switch to  https://github.com/snowyu/load-config-file.js, as it supports promises
+
+// used for require-all but could be modified for different "readLibs" function below
+let readLibsOptions = {
+  filter: /(.*)\.js$/,  // only .js files
+  recursive: false  // do not look in subdirectories - allows lib subdirectoies to be used for inactive or temp storage
+};
 
 module.exports = {
-  // loads any config type to object hash
+  // loads any config type to object hash, cson,yaml,json....
   load: function(filePath, callback) {
     return new Promise(function(resolve, reject) {
-      let object = read(filePath);
+      let object = readConfigFile(filePath);
+      if (object instanceof Error) {
+        console.log('problem loading configuration file: ', filePath);
+        reject(object.stack);
+      } else {
+        resolve(object);
+      }
+    });
+    done.nodeify(callback);
+  },
+  loadLibs: function(dirPath, options, callback) {
+    return new Promise(function(resolve, reject) {
+      let object = readLibs(dirPath,options);
       if (object instanceof Error) {
         reject(object.stack);
       } else {
@@ -18,6 +36,15 @@ module.exports = {
     done.nodeify(callback);
   },
   edit: {},
-  write:{},
-  view:{}
+  write: {},
+  view: {}
+}
+
+
+
+let requireall = require('require-all'); // TODO this is all fs sync so might want to do more than wrap i.e. redo to async/promise.
+// This abstracts the require-all library so can be replaced later with something async/promise
+function readLibs(dirname, options, callback) {
+  readLibsOptions.dirname = dirname;
+  return requireall(readLibsOptions)
 }
