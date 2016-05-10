c5d884af2d4106fe31c53b73fed8d739e97b40a0
David Kebler <d@kebler.net>
2016-05-10 11:18:14 -0700

    object view working

--- 4s.js
+++ 4s.js
@@ -1,8 +1,6 @@
 // 4S - Super Static Site Starter Entry Point
 'use strict';
 
-let getlibs = require('require-all');
-
 const repoPath = __dirname + '/';
 const configJSPath = require(repoPath + 'package').configjspath || repoPath + 'config/config.js'; // 'configjspath' is key in package.json.  If not set default is used
 const mainConfigPath = require(repoPath + 'package').configpath || repoPath + 'config/default.cson'; // 'configdir' is key in package.json.  If not set /config is the default
@@ -10,14 +8,14 @@ const mainConfigPath = require(repoPath + 'package').configpath || repoPath + 'c
 let config = require(configJSPath);
 
 init()
-.then(config => {
+  .then(config => {
     // configs loaded ok load the debugger here
-//    console.log('the configs from init\n', config)
+    //    console.log('the configs from init\n', config)
     // Build the cli
     //  console.log(config.lib.cli.build);
     let cli = config.lib.cli.build(config);
     // console.log(cli);
-    config.lib.cli.start(cli,config.cliData.cprompt);
+    config.lib.cli.start(cli, config.cliData.cprompt);
   })
   // Launch Cli here
   .catch(e => console.log('error', e));
@@ -28,10 +26,19 @@ init()
 //*******************************************
 
 
-// TODO Redo init with generators, or in future es8 await.
+// TODO Code this as a generator using co https://github.com/tj/co or eventually ES7 async/await when available
 //  see http://stackoverflow.com/questions/28250680/how-do-i-access-previous-promise-results-in-a-then-chain
 
 function init() {
+
+  // let initjobs = [];
+  // initjobs[0] = config.load(mainConfigPath);
+  // initjobs[1] = initjobs[0].then(mainConfig => {
+  //   return config.load(repoPath + mainConfig.cliConfigPath);
+  // });
+  // initjobs[2] = initjobs[0].then(mainConfig => {
+  //   return config.loadLibs(repoPath + mainConfig.dir.lib);
+  // });
 
   let getMainConfig = config.load(mainConfigPath);
   let getCliConfig = getMainConfig.then(mainConfig => {
@@ -45,7 +52,7 @@ function init() {
     .then(configs => {
       configs[0].cliData = configs[1]; // Add cli configuration data as a key in main config object
       configs[0].lib = configs[2]; //Add js libraries as a key to main config for easier access
-      configs[0].lib.config = config;  // Add in config file module for later use.
+      configs[0].lib.config = config; // Add in config file module for later use.
       return configs[0]; // now return merged main config file
     });
 }
--- config/config.js
+++ config/config.js
@@ -1,48 +1,46 @@
-// CONFIG module
-let Promise = require('promise');
-
-// allows single place to change module used for reading multiple config file types
-let readConfigFile = require('config').util.parseFile; // TODO switch to  https://github.com/snowyu/load-config-file.js, as it supports promises
-
-// used for require-all but could be modified for different "readLibs" function below
-let readLibsOptions = {
-  filter: /(.*)\.js$/,  // only .js files
-  recursive: false  // do not look in subdirectories - allows lib subdirectoies to be used for inactive or temp storage
-};
-
+// Configuration
 module.exports = {
   // loads any config type to object hash, cson,yaml,json....
-  load: function(filePath, callback) {
+  load: function(filePath) {
     return new Promise(function(resolve, reject) {
       let object = readConfigFile(filePath);
       if (object instanceof Error) {
-        console.log('problem loading configuration file: ', filePath);
+        console.log('ERROR: Problem loading configuration file: ', filePath);
         reject(object.stack);
       } else {
         resolve(object);
         return object;
       }
     });
-    done.nodeify(callback);
   },
-  loadLibs: function(dirPath, options, callback) {
+  loadLibs: function(dirPath, options) {
     return new Promise(function(resolve, reject) {
-      let object = readLibs(dirPath,options);
+      let object = readLibs(dirPath, options);
       if (object instanceof Error) {
         reject(object.stack);
       } else {
         resolve(object);
       }
     });
-    done.nodeify(callback);
   },
   edit: {},
   write: {},
   view: {}
+
 }
 
 
+// allows single place to change module used for reading multiple config file types
+let readConfigFile = require('config').util.parseFile; // TODO switch to  https://github.com/snowyu/load-config-file.js, as it supports promises
 
+
+
+// Read library directory
+// used for require-all but could be modified for different "readLibs" function below
+let readLibsOptions = {
+  filter: /(.*)\.js$/, // only .js files
+  recursive: false // do not look in subdirectories - allows lib subdirectoies to be used for inactive or temp storage
+};
 let requireall = require('require-all'); // TODO this is all fs sync so might want to do more than wrap i.e. redo to async/promise.
 // This abstracts the require-all library so can be replaced later with something async/promise
 function readLibs(dirname, options, callback) {
