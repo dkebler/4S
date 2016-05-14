980a3da36f4407182a03ba3d07245bda8648d71b
David Kebler <d@kebler.net>
2016-05-13 22:27:34 -0700

    debugging functioning

--- 4s.js
+++ 4s.js
@@ -1,15 +1,20 @@
 // 4S - Super Static Site Starter Entry Point
 'use strict';
 
-const repoPath = __dirname + '/';
-const configJSPath = require(repoPath + 'package').configjspath || repoPath + 'config/config.js'; // 'configjspath' is key in package.json.  If not set default is used
-const mainConfigPath = require(repoPath + 'package').configpath || repoPath + 'config/default.cson'; // 'configdir' is key in package.json.  If not set /config is the default
+global.RepoPath = __dirname + '/';
+global.Debug = require(require(RepoPath + 'package').configjspath || RepoPath + 'lib/debug.js');
+
+// let debug = require(require(RepoPath + 'package').configjspath || RepoPath + 'lib/debug.js');
+
+const configJSPath = require(RepoPath + 'package').configjspath || RepoPath + 'config/config.js'; // 'configjspath' is key in package.json.  If not set default is used
+const mainConfigPath = require(RepoPath + 'package').configpath || RepoPath + 'config/default.cson'; // 'configdir' is key in package.json.  If not set /config is the default
 
 let config = require(configJSPath);
 
 init()
   .then(config => {
     // configs loaded ok load the debugger here
+    Debug.L1('this is a test in main code');
     //    console.log('the configs from init\n', config)
     // Build the cli
     //  console.log(config.lib.cli.build);
@@ -42,17 +47,18 @@ function init() {
 
   let getMainConfig = config.load(mainConfigPath);
   let getCliConfig = getMainConfig.then(mainConfig => {
-    return config.load(repoPath + mainConfig.cliConfigPath);
+    return config.load(RepoPath + mainConfig.cliConfigPath);
   });
   let getLibs = getMainConfig.then(mainConfig => {
-    return config.loadLibs(repoPath + mainConfig.dir.lib);
+    return config.loadLibs(RepoPath + mainConfig.dir.lib);
   });
 
   return Promise.all([getMainConfig, getCliConfig, getLibs])
     .then(configs => {
       configs[0].cliData = configs[1]; // Add cli configuration data as a key in main config object
       configs[0].lib = configs[2]; //Add js libraries as a key to main config for easier access
-      configs[0].lib.config = config; // Add in config file module for later use.
+      configs[0].lib.config = config; // Add in config js module for later use.
+      configs[0].repoPath = RepoPath;  // add repo root path in case it is needed for absolute references
       return configs[0]; // now return merged main config file
     });
 }
